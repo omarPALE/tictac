d13a40c0e11578afb94f636d9bec608cca51d5ec
omarPALE <omar.alratrot@yahoo.com>
2023-04-08 14:58:24 +0300

    Initial commit

---
+++ tic-tac.js
@@ -0,0 +1,152 @@
+const squares = document.querySelectorAll('.square');
+const winner = document.querySelector('#winner');
+const twoplayer = document.querySelector('.twoPlayer');
+const ai = document.querySelector('.ai');
+const rows = document.querySelectorAll('.row');
+console.log(rows);
+let board =  [[],[],[]];
+
+function fullBoard(){
+    for (let i = 0; i < 9; i++) {
+        // get the content of the current div element
+        const content = squares[i].innerHTML;
+        // calculate the row and column indexes in the array
+        var row = Math.floor(i / 3);
+        var col = i % 3;
+        // add the content to the array at the appropriate position
+        board[row][col] = content;
+    }
+
+
+// if(id <=2){
+//         board[0][id] = document.getElementById(id).innerHTML ;
+// }
+// else if (id > 2 && id <= 5) {
+//     board[1][id-3] = document.getElementById(id).innerHTML ;
+// }
+// else{
+//     board[2][id-6] = document.getElementById(id).innerHTML ;
+// }
+}
+
+fullBoard();
+for(let i = 0 ; i<3;i++){
+    for(let j = 0 ; j<3;j++){
+        console.log(board[i][j]);
+    }
+}
+function player(human, value) {
+    this.human = true;
+    this.value = 'X';
+}
+
+let playerr = new player(true, 'X');
+
+twoplayer.addEventListener("click", function() {
+    winner.textContent = `Two Player`;
+    squares.forEach((square) => {
+        square.addEventListener('click', handleClick,{ once: true});
+    });
+    console.log("TWO player clicked!");
+});
+
+function handleClick(event) {
+    // console.log(playerr.value);
+    const square = event.target;
+    square.innerHTML = playerr.value;
+    square.style.color = playerr.value === 'X' ? '#1abc9c' : '#2c3e50';
+    // nextTurn();
+    checkWin();
+
+    playerr.value = playerr.value === 'X' ? 'O' : 'X';
+}
+
+ai.addEventListener("click", function() {
+    winner.textContent = `AI GAME`;
+    squares.forEach((square) => {
+        square.addEventListener('click', handleClick2, {once: true});
+    });
+    console.log("AI clicked!");
+});
+
+function handleClick2(event) {
+    playerr.value = 'X';
+    const square1 = event.target;
+    square1.innerHTML = playerr.value;
+    square1.style.color = playerr.value === 'O' ? '#1abc9c' : '#2c3e50';
+    checkWin();
+    nextTurn();
+    //playerr.value = playerr.value === 'O' ? 'X' : 'O';
+}
+
+function nextTurn() {
+    playerr.value = 'O';
+    let available=[];
+    for(let i = 0; i<squares.length ;i++){
+        const content = squares[i].innerHTML;
+        if(content === ''){
+            available[i]=squares[i].id;
+            content.innerHTML = "O";
+            // let score = minmax()
+        }
+    }
+    // for(let g = 0; g = available.length; g++){
+    //     fullBoard(available[g]);
+    // }
+    // for(let i = 0 ; i<3;i++){
+    //     for(let j = 0 ; j<3;j++){
+    //         console.log(board[i][j]);
+    //     }
+    // }
+
+    let move = random(available);
+    const randomdiv = document.getElementById(move);
+    if (randomdiv.innerHTML != null)
+        randomdiv.innerHTML = "O";
+    console.log(randomdiv);
+    playerr.value='O'
+    playerr.human = false;
+    checkWin()
+
+}
+
+function random(available) {
+    let randomIndex = Math.floor(Math.random() * available.length);
+    return available[randomIndex];
+    return undefined;
+}
+
+function checkWin() {
+    const rows = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < rows.length; i++) {
+        const [a, b, c] = rows[i];
+        if (squares[a].innerHTML === playerr.value &&
+            squares[b].innerHTML === playerr.value &&
+            squares[c].innerHTML === playerr.value) {
+            winner.textContent = `${playerr.value} wins!`;
+            return true;
+        }
+
+    }
+    return false;
+}
+
+function restart() {
+    squares.forEach((square) => {
+        square.innerHTML = '';
+        square.style.color = '#333';
+        winner.textContent = `Playing`;
+
+    });
+    playerr.value = 'X';
+
+}
