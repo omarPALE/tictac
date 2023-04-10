727ffda14b322faee8900a511b7c1b41ab7abe7b
omarPALE <omar.alratrot@yahoo.com>
2023-04-11 00:01:47 +0300

    try to solve hard level

--- tic-tac.js
+++ tic-tac.js
@@ -4,7 +4,10 @@ const twoplayer = document.querySelector('.twoPlayer');
 const ai = document.querySelector('.ai');
 const easy = document.querySelector(".easy")
 const rows = document.querySelectorAll('.row');
-console.log(rows);
+const humanPlayer = 'X';
+const aiPlayer = 'O';
+let currentPlayer = humanPlayer;
+
 let board =  [[],[],[]];
 
 
@@ -50,6 +53,7 @@ twoplayer.addEventListener("click", function() {
 });
 
 function handleClick(event) {
+    fullBoard();
     console.log("hiiii");
     if (!playerr.win) {
         const square = event.target;
@@ -57,12 +61,12 @@ function handleClick(event) {
         square.style.color = playerr.value === 'X' ? '#1abc9c' : '#2c3e50';
         checkWin();
         playerr.value = playerr.value === 'X' ? 'O' : 'X';
-        fullBoard();
-        for(let i=0 ;i<board.length; i++) {
-            for (let j = 0; j < board.length; j++) {
-                console.log(board[i][j]);
-            }
-        }
+        // fullBoard();
+        // for(let i=0 ;i<board.length; i++) {
+        //     for (let j = 0; j < board.length; j++) {
+        //         console.log(board[i][j]);
+        //     }
+        // }
     }
 }
 
@@ -123,35 +127,49 @@ function handleClick2(event) {
         square1.innerHTML = playerr.value;
         console.log(square1.innerHTML);
         square1.style.color = playerr.value === 'O' ? '#1abc9c' : '#2c3e50';
+        fullBoard();
         checkWin();
-        nextTurn();
+        nextTurn(board);
     }
     //playerr.value = playerr.value === 'O' ? 'X' : 'O';
 }
 
-function nextTurn() {
-    playerr.value = 'O';
-    let bestscore = -Infinity;
-    let available=[9];
-    for(let i = 0; i<squares.length ;i++){
-        const content = squares[i].innerHTML;
-        if(content === ''){
-            squares[i]= playerr.value;
-            //content.innerHTML = "O";   OLD DESIGN
-             let score = alphBeta(squares);
-             if(score > bestscore){
-                 bestscore =score;
+function nextTurn(board) {
+    // fullBoard();
+    console.log("in next turn");
+    let bestScore = -Infinity;
+    let bestMove;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+          console.log("after for ");
 
-             }
+          if (board[i][j] === '') {
+          board[i][j] = aiPlayer;
+          const score = minimax(board, 0, false, -Infinity, Infinity);
+          board[i][j] = '';
+          console.log("Before if ");
+          if (score > bestScore) {
+            bestScore = score;
+            bestMove = { i , j };
+            console.log("best move =  i is :" + i + "j is :"+ j);
+          }
         }
+      }
     }
-    let move = random(available);
-        const randomdiv = document.getElementById(move);
-            randomdiv.innerHTML = "O";
-            playerr.value = 'O'
-            playerr.human = false;
-            checkWin()
-   }
+    board[bestMove.i][bestMove.j] = aiPlayer;
+    let currentPlayer = humanPlayer;
+    }
+
+
+
+
+   //  let move = random(available);
+   //      const randomdiv = document.getElementById(move);
+   //          randomdiv.innerHTML = "O";
+   //          playerr.value = 'O'
+   //          playerr.human = false;
+   //          checkWin();
+   // }
 
 
 
@@ -194,4 +212,93 @@ function restart() {
 
 function random(available) {
     return  Math.floor(Math.random() * available.length);
+}
+
+function evaluate(player) {
+  if (checkWin() && player.value ===aiPlayer) {
+    return 1;
+  }
+  if (checkWin() && player.value === humanPlayer) {
+    return -1;
+  }
+  return 0;
+}
+
+function checkWin2(board , player) {
+    for (let i = 0; i < 3; i++) {
+    if (board[i][0] === player && board[i][1] === player && board[i][2] === player) {
+        // winner.textContent = `${playerr.value} wins!`;
+        // playerr.win= true;
+        return true;
+    }
+    if (board[0][i] === player && board[1][i] === player && board[2][i] === player) {
+        // winner.textContent = `${playerr.value} wins!`;
+        // playerr.win= true;
+        return true;
+    }
+  }
+  if (board[0][0] === player && board[1][1] === player && board[2][2] === player) {
+      // winner.textContent = `${playerr.value} wins!`;
+      // playerr.win= true;
+      return true;
+  }
+  if (board[0][2] === player && board[1][1] === player && board[2][0] === player) {
+      // winner.textContent = `${playerr.value} wins!`;
+      // playerr.win= true;
+      return true;
+  }
+  return false;
+}
+
+function isBoardFull(board) {
+fullBoard();
+for (let i= 0 ; i<3;i++){
+    for(let j = 0 ; j <3 ; j++){
+        if(board[i][j] === '')
+            return false ;
+    }
+}
+return true;
+}
+
+function minimax(board, depth, isMaximizingPlayer, alpha, beta) {
+  const score = evaluate(playerr);
+  if (score !== 0 || isBoardFull(board)) {
+    return score;
+  }
+  if (isMaximizingPlayer) {
+    let bestScore = -Infinity;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (board[i][j] === '') {
+          board[i][j] = aiPlayer;
+          const currentScore = minimax(board, depth + 1, false, alpha, beta);
+          board[i][j] = '';
+          bestScore = Math.max(bestScore, currentScore);
+          alpha = Math.max(alpha, bestScore);
+          if (beta <= alpha) {
+            break;
+          }
+        }
+      }
+    }
+    return bestScore;
+  } else {
+    let bestScore = Infinity;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (board[i][j] === '') {
+          board[i][j] = humanPlayer;
+          const currentScore = minimax(board, depth + 1, true, alpha, beta);
+          board[i][j] = '';
+          bestScore = Math.min(bestScore, currentScore);
+          beta = Math.min(beta, bestScore);
+          if (beta <= alpha) {
+            break;
+          }
+        }
+      }
+    }
+    return bestScore;
+  }
 }
